137f4caaf6c610d3eb7fb893d63ed308b113e144
Cody Nicholson <codynicholson96@gmail.com>
2020-05-06 21:38:59 -0500

    reset logs and made logging less frequent

--- src/collect_iss_data.py
+++ src/collect_iss_data.py
@@ -11,18 +11,17 @@ from sqlalchemy import create_engine
 files = [f for f in glob.glob("/home/pi/ISS_Tracking_Data_Collection_Project/logs/*.txt", recursive=True)]
 log_count = str(len(files)+1)
 print(f"Log: {log_count}")
-
 now = datetime.datetime.now()
 timestamp = str(now.strftime("%Y-%m-%d-%H-%M-%S"))
 print(f"timestamp is: {timestamp}")
 
-with open(f"/home/pi/ISS_Tracking_Data_Collection_Project/logs/log{log_count}-{timestamp}.txt","w+") as file:
-	file.write("LOG!!!\n")
-	file.write(f"timestamp: {timestamp}\n")
-	file.write(f"datetime: {str(now)}\n")
-	file.write(f"logs: {log_count}\n")
-	file.write("End Log\n\n")
-	file.close()
+#with open(f"/home/pi/ISS_Tracking_Data_Collection_Project/logs/log{log_count}-{timestamp}.txt","w+") as file:
+#	file.write("ISS Tracking Data Collection Project Log\n")
+#	file.write(f"timestamp: {timestamp}\n")
+#	file.write(f"datetime: {str(now)}\n")
+#	file.write(f"logs: {log_count}\n")
+#	file.write("End Log\n\n")
+#	file.close()
 
 # Get ISS Position Data
 iss_url = "http://api.open-notify.org/iss-now.json"
