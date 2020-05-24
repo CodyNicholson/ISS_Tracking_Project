98d92577462a2954a48b1e8cadcfaa6a7fb1f34b
Cody Nicholson <codynicholson96@gmail.com>
2020-05-24 06:48:53 -0500

    refactored get data call

---
+++ app/data.py
@@ -0,0 +1,35 @@
+from config import host, port, database, user, password
+import datetime
+from sqlalchemy.orm import Session
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+class ISS_Data_Point(Base):
+    __tablename__ = 'iss_data_table'
+    iss_timestamp = Column(String(50), primary_key=True)
+    iss_lat = Column(String(20))
+    iss_lon = Column(String(20))
+    num_description = Column(String(255))
+    weather_description = Column(String(255))
+    weather_temp = Column(String(10))
+    country_alpha_code = Column(String(10))
+    country_name = Column(String(50))
+    country_borders = Column(String(255))
+    country_flag_url = Column(String(255))
+    country_capital = Column(String(50))
+
+engine = create_engine(f"postgresql://{user}:{password}@{host}/{database}")
+connection = engine.connect()
+metadata = MetaData()
+Base.metadata.create_all(connection)
+session = Session(bind=engine)
+
+def getData():
+    resultProxy = connection.execute('SELECT * FROM public.iss_data_table ORDER BY iss_timestamp ASC FETCH FIRST 100 ROWS ONLY;')
+    rowDict, dataList = {}, []
+    for rowProxy in resultProxy:
+        for column, value in rowProxy.items():
+            rowDict = {**rowDict, **{column: value}}
+        dataList.append(rowDict)
+    return dataList
