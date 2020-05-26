6f8bb4576ad890ba529969baf07fe3d4c91f4591
Cody Nicholson <codynicholson96@gmail.com>
2020-05-26 18:35:14 -0500

    Click popups to close

--- flask_app/static/js/app.js
+++ flask_app/static/js/app.js
@@ -42,10 +42,14 @@ function toggleStyle() {
 var popup = L.popup();
 function onMapClick(e) {
     popup.setLatLng(e.latlng)
-        .setContent("You clicked the map at:<br>" + e.latlng.toString())
+        .setContent(`<div class="popup" onclick="closeAllPopups()">You clicked the map at:<br>${e.latlng.toString()}</div>`)
         .openOn(mymap);
 }
 mymap.on('click', onMapClick);
+
+function closeAllPopups() {
+    mymap.closePopup();
+}
 
 var markers = [];
 var viewMarkers = true;
@@ -65,10 +69,10 @@ function getMarkersAndDrawLines() {
             }
             var marker = L.marker([row.iss_lat, row.iss_lon], markerOptions).addTo(mymap);
             if (row.country_name == "") {
-                marker.bindPopup(`<b>Lat, Lon:</b> (${row.iss_lat}, ${row.iss_lon})<br>${timeConverter(row.iss_timestamp)}<br><b>Temp:</b> ${row.weather_temp} °F<br><b>Weather:</b> ${row.weather_description}<br><b>Random Number Fact:</b> ${row.num_description}`).openPopup();
+                marker.bindPopup(`<div class="popup" onclick="closeAllPopups()"><b>Lat, Lon:</b> (${row.iss_lat}, ${row.iss_lon})<br>${timeConverter(row.iss_timestamp)}<br><b>Temp:</b> ${row.weather_temp} °F<br><b>Weather:</b> ${row.weather_description}<br><b>Random Number Fact:</b> ${row.num_description}</div>`).openPopup();
             } else {
                 var borderingCountries = row.country_borders.substring(1, row.country_borders.length-1).split(",").join(", ");
-                marker.bindPopup(`<img src="${row.country_flag_url}" alt="Country Flag" width="100%"/><br><b>Lat, Lon:</b> (${row.iss_lat}, ${row.iss_lon})<br>${timeConverter(row.iss_timestamp)}<br><b>Temp:</b> ${row.weather_temp} °F<br><b>Weather:</b> ${row.weather_description}<br><b>Country:</b> ${row.country_name} (${row.country_alpha_code})<br><b>Capital:</b> ${row.country_capital}<br><b>Borders:</b> ${borderingCountries}<br><b>Random Number Fact:</b> ${row.num_description}`).openPopup();
+                marker.bindPopup(`<div class="popup" onclick="closeAllPopups()"><img src="${row.country_flag_url}" alt="Country Flag" width="100%"/><br><b>Lat, Lon:</b> (${row.iss_lat}, ${row.iss_lon})<br>${timeConverter(row.iss_timestamp)}<br><b>Temp:</b> ${row.weather_temp} °F<br><b>Weather:</b> ${row.weather_description}<br><b>Country:</b> ${row.country_name} (${row.country_alpha_code})<br><b>Capital:</b> ${row.country_capital}<br><b>Borders:</b> ${borderingCountries}<br><b>Random Number Fact:</b> ${row.num_description}</div>`).openPopup();
             }
             markers.push(marker);
         });
