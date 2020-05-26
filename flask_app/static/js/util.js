58fb30e5f2787181940013e5c3f535cd79ea3af4
Cody Nicholson <codynicholson96@gmail.com>
2020-05-26 10:10:53 -0500

    Changed military time to standard

--- flask_app/static/js/util.js
+++ flask_app/static/js/util.js
@@ -1,13 +1,26 @@
 function timeConverter(UNIX_timestamp){
     var a = new Date(UNIX_timestamp * 1000);
-    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    //var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var amPM = "AM";
     var year = a.getFullYear();
-    var monthName = months[a.getMonth()];
+    //var monthName = months[a.getMonth()];
     var month = a.getMonth() + 1;
-    var date = a.getDate();
+    var day = a.getDate();
     var hour = a.getHours();
-    var min = a.getMinutes();
-    var sec = a.getSeconds();
-    var time = '<b>Date:</b> ' + month + '/' + date + '/' + year + '<br><b>Time:</b> ' + hour + ':' + min;
+    if (hour > 12) {
+        hour -= 12;
+        amPM = "PM";
+    }
+    var min = zeroPadMinutes(a.getMinutes());
+    //var sec = a.getSeconds();
+    var time = `<b>Time:</b> ${hour}:${min} ${amPM}<br><b>Date:</b> ${month}/${day}/${year}`;
     return time;
 };
+
+function zeroPadMinutes(value) {
+    if(value < 10) {
+        return '0' + value;
+    } else {
+        return value;
+    }
+}
