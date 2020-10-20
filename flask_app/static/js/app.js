0704e777d29628d9f6632c4e8d44a98b5ad2b464
Cody Nicholson <codynicholson96@gmail.com>
2020-10-20 18:41:38 -0500

    Added changes

--- flask_app/static/js/app.js
+++ flask_app/static/js/app.js
@@ -328,6 +328,7 @@ function getAverageIssMph() {
 function getUniqueCountryNameCounts() {
     $.getJSON("/country-count", function(uniqueCountryNameCounts) {
         console.log(uniqueCountryNameCounts);
+        return uniqueCountryNameCounts;
     });
 }
 
@@ -385,3 +386,41 @@ setInterval(function() {
 // Graph average temperature per country visited by the ISS in bar graph
 // create json file with sample data to be used when database connection fails
 // response.setHeader("Set-Cookie", "HttpOnly;Secure;SameSite=Strict");
+
+
+
+// Dataset we will be using to set the height of our rectangles.
+var booksReadThisYear = [17, 23, 20, 34];
+
+// Setting the dimensions for the SVG container
+var svgHeight = 600;
+var svgWidth = 400;
+
+var svg = d3
+  .select("#svg-area")
+  .append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth);
+
+// svgGroup now refers to the `g` (group) element appended.
+// The SVG group would normally be aligned to the top left edge of the chart.
+// Now it is offset to the right and down using the transform attribute
+var svgGroup = svg.append("g")
+  .attr("transform", "translate(50, 100)");
+
+// Selects all rectangles currently on the page - which is none - and binds our dataset to them. If there are no rectangles, it will append one for each piece of data.
+svgGroup.selectAll("rect")
+  .data(getUniqueCountryNameCounts().map(c => {return c.country_count}))
+  .enter()
+  .append("rect")
+  .attr("width", 10)
+  .attr("height", function(data) {
+    return data;
+  })
+  .attr("x", function(data, index) {
+    return index * 30;
+  })
+  .attr("y", function(data) {
+    return 600 - data * 10;
+  })
+  .attr("class", "bar");
