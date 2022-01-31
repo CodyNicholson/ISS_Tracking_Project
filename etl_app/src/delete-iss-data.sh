e64ce291e6537046d9f6ba7708cbd33a434ad8f5
Cody Nicholson <codynicholson96@gmail.com>
2022-01-30 18:49:13 -0600

    Added cron job docs and delete data script

---
+++ etl_app/src/delete-iss-data.sh
@@ -0,0 +1,1 @@
+PGPASSWORD=08d9dca91c773c5f0c129696378d546bcb501c4686ee6696a3106ed685f74b5d psql -h ec2-3-224-23-0.compute-1.amazonaws.com -d drut1ogg4206h -U dcgyftmeejtile -c "SELECT * FROM public.iss_data_table ORDER BY iss_timestamp ASC FETCH FIRST 1400 ROWS ONLY"
